b85620747d6a6a195d11f55ac2e442f5c825b945
Jithin S <jithinyakkara@gmail.com>
2026-01-17 12:45:03 +0530

    Add comment for Todo model fields
    Added a comment to indicate the purpose of the Todo model.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db
 
 class Todo(db.Model):
+    #todo fields
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Boolean, default=False)
